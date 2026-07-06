7ef739941ab361249c03dba2a82716aa093824a1
Thomas Eisenhauer <gobliteke@proton.me>
2026-07-06 14:26:08 +0200

    Rename and update prom configuration file

--- src/services/loadConfig.ts
+++ src/services/loadConfig.ts
@@ -1,5 +1,5 @@
 export async function loadConfig(): Promise<any> {
-    const response = await fetch('/config/config2.json');
+    const response = await fetch('/config/proms.json');
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
